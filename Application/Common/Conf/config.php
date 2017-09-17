a6e8c9a29b48e74ea7bc044b50e958b10ab047a3
s-qiujing <s-qiujing@lenovox1-PC.sct.sh.cn>
2017-09-17 23:31:17 +0800

    修改数据库名,数据库密码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,9 +6,9 @@ return array(
     //数据库配置信息
    'DB_TYPE'   => 'mysql', // 数据库类型
    'DB_HOST'   => 'localhost', // 服务器地址
-   'DB_NAME'   => 'wms', // 数据库名
+   'DB_NAME'   => 'myERP', // 数据库名
    'DB_USER'   => 'root', // 用户名
-   'DB_PWD'    => '', // 密码
+   'DB_PWD'    => 'sys123', // 密码
    'DB_PORT'   => 3306, // 端口
    'DB_PARAMS' =>  array(), // 数据库连接参数
    'DB_PREFIX' => 'wms_', // 数据库表前缀 
@@ -23,7 +23,6 @@ return array(
     // 'SESSION_EXPIRE' => 60*60, //Memcache的session信息有效时间
     // 'PERSISTENTID'=>'tp',//可选
     // 'MEMECACHED_WEIGHT' => array(33,67),//可选    
-    
     
     
 );
