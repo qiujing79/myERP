ae82f6e3af3227039f57ddfe1705133978bb2a5b
s-qiujing <s-qiujing@lenovox1-PC.sct.sh.cn>
2017-10-11 10:23:36 +0800

    test 密码密码

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,7 +8,7 @@ return array(
    'DB_HOST'   => 'localhost', // 服务器地址
    'DB_NAME'   => 'myERP', // 数据库名
    'DB_USER'   => 'root', // 用户名
-   'DB_PWD'    => 'sys123', // 密码
+   'DB_PWD'    => 'sys123', // 密码密码
    'DB_PORT'   => 3306, // 端口
    'DB_PARAMS' =>  array(), // 数据库连接参数
    'DB_PREFIX' => 'wms_', // 数据库表前缀 
